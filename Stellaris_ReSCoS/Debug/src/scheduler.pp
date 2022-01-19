7db73207f072dea278398cb9e9195a4ac9e4276b
chrimenz <94612190+chrimenz@users.noreply.github.com>
2022-01-19 22:41:51 +0100

    Delete Stellaris_ReSCoS/Debug directory

--- Stellaris_ReSCoS/Debug/src/scheduler.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-src/scheduler.obj: ../src/scheduler.c
-src/scheduler.obj: ../src/inc/scheduler.h
-
-../src/scheduler.c: 
-../src/inc/scheduler.h: 
--- Stellaris_ReSCoS/Debug/src/startup_ccs.pp
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-src/startup_ccs.obj: ../src/startup_ccs.c
-
-../src/startup_ccs.c: 
